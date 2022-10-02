b196a9709fc86e38bb4c2ff0d43c371338a9517c
Minh Thach <tatsumiikun1@gmail.com>
2022-10-02 16:57:13 -0400

    inital commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,37 +12,36 @@ fetch('https://hotels4.p.rapidapi.com/locations/v2/search?query=new%20york', opt
 
     console.log(result);
 
+    for (let i = 0; i < (result.suggestions).length; i++) {
+      for (j = 0; j < result.suggestions [i].entities.length; j++) {
+        let desId = result.suggestions[i].entities[j].destinationId;
 
-
-    // let desId2 = result.suggestions[1].entities[1].destinationId;
-    // let desId3 = result.suggestions[1].entities[0].destinationId;
-    // let desId4 = result.suggestions[1].entities[1].destinationId;
-
-    for (let i = 0; i < Object.keys(result.suggestions[1].entities).length; i++) {
-      let desId = result.suggestions[1].entities[i].destinationId;
-
-      const options = {
-        method: 'GET',
-        headers: {
-          'X-RapidAPI-Key': '86214fd43amshd95edfa25068fadp1277c5jsnd6668ed6eebd',
-          'X-RapidAPI-Host': 'hotels4.p.rapidapi.com'
-        }
-      };
-      let url = `https://hotels4.p.rapidapi.com/properties/get-details?id=${desId}&checkIn=2022-11-08&8&checkOut=2022-11-09&&currency=USD&locale=en_US`
-      fetch(url, options)
+        let url = `https://hotels4.p.rapidapi.com/properties/get-details?id=${desId}&checkIn=2022-11-08&8&checkOut=2022-11-09&&currency=USD&locale=en_US`
+        fetch(url, options)
         .then(response => response.json())
         .then(function (result2) {
-          console.log(result2);
-          Object.keys()
+        console.log(result2);
 
-
-
-
-        })
-        .catch(err => console.error(err));
-
+        }
+      )}
     }
-
   })
+      // let desId = result.suggestions[1].entities[i].destinationId;
+
+      // const options = {
+      //   method: 'GET',
+      //   headers: {
+      //     'X-RapidAPI-Key': '86214fd43amshd95edfa25068fadp1277c5jsnd6668ed6eebd',
+      //     'X-RapidAPI-Host': 'hotels4.p.rapidapi.com'
+      //   }
+      // };
+      // let url = `https://hotels4.p.rapidapi.com/properties/get-details?id=${desId}&checkIn=2022-11-08&8&checkOut=2022-11-09&&currency=USD&locale=en_US`
+      // fetch(url, options)
+      //   .then(response => response.json())
+      //   .then(function (result2) {
+      //     console.log(result2);
+      //     Object.keys()
+      //   })
+      //   .catch(err => console.error(err));
   .catch(err => console.error(err));
 
